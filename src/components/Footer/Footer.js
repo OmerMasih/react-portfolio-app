566841ae6eef71e7bfc28c65de10513a5a48f173
OmerMasih <masihomer123@gmail.com>
2023-06-04 17:29:20 -0700

    Creating the footer and work files

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,30 @@
+import { ReactComponent as GITHUB } from "../../Resources/icons/github.svg";
+import { ReactComponent as INSTAGRAM } from "../../Resources/icons/instagram.svg";
+import { ReactComponent as TWITTER } from "../../Resources/icons/twitter.svg";
+import { ReactComponent as LINKEDIN } from "../../Resources/icons/linkedin.svg";
+import { ReactComponent as Facebook } from "../../Resources/icons/facebook.svg";
+
+const Footer = [
+  {
+    link: "https://github.com/OmerMasih",
+    icon: GITHUB,
+  },
+  {
+    link: "https://www.instagram.com/masihullah_omar/",
+    icon: INSTAGRAM,
+  },
+  {
+    link: "https://twitter.com/@MasihOmer",
+    icon: TWITTER,
+  },
+  {
+    link: "https://www.linkedin.com/in/masih-omer-016b12131",
+    icon: LINKEDIN,
+  },
+
+  {
+    link: "https://www.facebook.com/",
+    icon: Facebook,
+  },
+];
+export default Footer;
